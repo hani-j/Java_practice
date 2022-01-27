bfcb46c6536908cd00f2039824b2ce8a51ff7a37
hani <gkdms4248@gmail.com>
2022-01-27 23:15:56 +0900

    casting_02

---
+++ src/main/java/CastingExample_02.java
@@ -0,0 +1,15 @@
+public class CastingExample_02 {
+    public static void main(String[] args){
+        int intValue = 103029770;
+        byte byteValue = (byte)intValue;
+        int intValue2 = 10;
+        double doubleValue = 5.5;
+
+        System.out.println("byteValue=" + byteValue);
+        System.out.println("intValue=" + intValue);
+
+        double result = intValue2 + doubleValue;
+        System.out.println(result);
+        System.out.println(intValue2);
+    }
+}
