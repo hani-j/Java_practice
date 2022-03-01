2944d5db0f9142487b2a2ae804fcc08e558d6194
hani <gkdms4248@gmail.com>
2022-03-01 20:31:40 +0900

    49_02

--- src/main/java/ClassEx14_49/ClassVar_01.java
+++ src/main/java/ClassEx14_49/ClassVar_01.java
@@ -2,7 +2,11 @@ package ClassEx14_49;
 
 class InstCnt{
     static int instNum=0;
-    //int instNum=0;
+    // int instNum=0;
+    // static : 모든 변수가 선언된 클래스의 모든 객체가 공유
+    //          메모리 공간에 하나만 존재, 어디서나 접근이 가능
+    //          static 변수도 public 으로 선언되어야 함
+    //          정적필드, 정적메서드, 정적초기화 블록에서 선언 가능
 
     public InstCnt() {
         instNum++;
---
+++ src/main/java/ClassEx14_49/StaticFieldExample_02.java
@@ -0,0 +1,27 @@
+package ClassEx14_49;
+
+class Accumulator {
+    int total = 0;
+    static int grandTotal = 0;
+    // 객체 생성과 상관없이 사용, 객체가 없어져도 메모리에 유지
+
+    void accumulate(int amount) {
+        total += amount;
+        grandTotal += amount;
+    }
+}
+
+public class StaticFieldExample_02 {
+    public static void main(String[] args) {
+        Accumulator obj1 = new Accumulator();
+        Accumulator obj2 = new Accumulator();
+
+        obj1.accumulate(10);
+        obj2.accumulate(20);
+
+        System.out.println("obj1.total = " + obj1.total);
+        System.out.println("obj1.grandTotal = " + obj1.grandTotal);
+        System.out.println("obj2.total = " + obj2.total);
+        System.out.println("obj2.grandTotal = " + obj2.grandTotal);
+    }
+}
