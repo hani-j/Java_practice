d7d499bf0c6ab9b5724be47030fa3c9cc0a7a89a
hani <gkdms4248@gmail.com>
2022-03-08 19:45:34 +0900

    52

---
+++ src/main/java/ClassEx17_52/Student.java
@@ -0,0 +1,10 @@
+package ClassEx17_52;
+
+public class Student extends People {
+    public int studentNo;
+
+    public Student(String name, String ssn, int studentNo) {
+        super(name, ssn);
+        this.studentNo = studentNo;
+    }
+}
