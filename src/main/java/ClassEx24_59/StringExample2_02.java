4ce0029e63d2e6468b535cad97a017f3b93c31f5
Haeun Jeong <gkdms4248@gmail.com>
2022-04-06 14:02:56 +0000

    59_02

---
+++ src/main/java/ClassEx24_59/StringExample2_02.java
@@ -0,0 +1,17 @@
+public class StringExample2_02 {
+
+	public static void main(String[] args) {
+		//String str1="자바";
+		//String str2="자바";
+		
+		String str1=new String("자바");
+		String str2=new String("자바");
+		
+		if(str1 == str2)
+			System.out.println("같음");
+		else
+			System.out.println("다름");
+		
+	}
+
+}
