5485f4e1671475aae18a60abfa048c9dd0878f8e
Haeun Jeong <gkdms4248@gmail.com>
2022-04-07 13:35:04 +0000

    59_03

---
+++ src/main/java/ClassEx24_59/StringExample3_03.java
@@ -0,0 +1,18 @@
+public class StringExample3_03 {
+
+	public static void main(String[] args) {
+		//String str1="자바";
+		//String str2="자바";
+				
+		String str1=new String("자바");
+		String str2=new String("자바");
+				
+		if(str1.equals(str2)) // 참조변수가 가리키는 값을 비교
+			// String equals : 내용비교
+			System.out.println("같음");
+		else
+			System.out.println("다름");
+		
+	}
+
+}         
