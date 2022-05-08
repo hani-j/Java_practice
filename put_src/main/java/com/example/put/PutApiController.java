60b770b60e8e0bdf03c6e251a1841f0ab4c0d7e1
hani <gkdms4248@gmail.com>
2022-05-08 13:06:07 +0900

    put

---
+++ put_src/main/java/com/example/put/PutApiController.java
@@ -0,0 +1,24 @@
+package com.example.put;
+
+import com.example.put.dto.PutRequestDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PutApiController {
+
+//    @PutMapping("/put")
+////    public void put(@RequestBody PutRequestDto requestDto){
+////        System.out.println(requestDto);
+////    }
+////    response
+//    public PutRequestDto put(@RequestBody PutRequestDto requestDto){
+//        System.out.println(requestDto);
+//        return requestDto;
+//    }
+    @PutMapping("/put/{userId}")
+    public PutRequestDto put(@RequestBody PutRequestDto requestDto, @PathVariable(name = "userId") Long id){
+        System.out.println(id);
+        return requestDto;
+    }
+}
