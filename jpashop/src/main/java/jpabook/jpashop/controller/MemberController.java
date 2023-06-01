3963e0114f9184b02374686ec983ae015e499fa0
Haeun Jeong <gkdms4248@gmail.com>
2023-06-01 17:11:36 +0900

    controller

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String crateForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+    // form 으로 받아서 정제된 데이터를 넘기는 것을 추천 (실전)
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        } // Error 가 있을 때
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        // ctrl + T
+        model.addAttribute("members", memberService.findMembers());
+//        List<Member> members = memberService.findMembers();
+//        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
